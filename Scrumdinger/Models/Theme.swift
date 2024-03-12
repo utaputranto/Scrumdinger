a407d1ac8b3dcf07a319d622cb04da0357e541dc
Putra <utaputranto@gmail.com>
2024-03-12 22:44:24 +0700

    Passing data with bindings

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-enum Theme: String {
+enum Theme: String, CaseIterable, Identifiable {
     case bubblegum
     case buttercup
     case indigo
@@ -37,5 +37,8 @@ enum Theme: String {
     }
     var name: String {
         rawValue.capitalized
+    }
+    var id: String {
+        name
     }
 }
---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,28 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Ahmad Tri Putranto on 12/03/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+struct ThemeView_Preview: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .buttercup)
+    }
+}
